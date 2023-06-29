eed714e952e04ccd3d865c7ba2226094f5fd3174
bingowo <83755226+bingowo@users.noreply.github.com>
2023-06-29 14:59:17 +0800

    update

--- examples/gcn_cora.py
+++ examples/gcn_cora.py
@@ -3,5 +3,5 @@ import opengsl
 conf = opengsl.config.load_conf(method="gcn", dataset="cora")
 dataset = opengsl.data.Dataset("cora", n_splits=1, feat_norm=conf.dataset['feat_norm'])
 solver = opengsl.method.GCNSolver(conf,dataset)
-exp = opengsl.ExpManager(solver, n_runs = 10)
-exp.run()
+exp = opengsl.ExpManager(solver)
+exp.run(n_runs = 10)
