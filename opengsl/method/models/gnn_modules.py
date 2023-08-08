f4d6470b4138453662f8516fc168f4774862676f
bingowo <1826097806@qq.com>
2023-08-09 07:31:25 +0800

    add GIN

--- opengsl/method/models/gnn_modules.py
+++ opengsl/method/models/gnn_modules.py
@@ -262,3 +262,38 @@ class GAT(nn.Module):
             x = F.elu(x)
         x = self.convs[-1](x, edge_index)
         return x.squeeze(1)
+
+
+class GIN(nn.Module):
+    def __init__(self, n_feat, n_hidden, n_class, n_layers=3, learn_eps=False, spmm_type=0):
+        super(GIN, self).__init__()
+        self.n_layers = n_layers
+        self.learn_eps = learn_eps
+        self.eps = nn.Parameter(torch.zeros(self.n_layers-1))
+        self.spmm = [torch.spmm, torch.sparse.mm][spmm_type]
+        
+        self.mlps = nn.ModuleList()
+        if n_layers == 1:
+            self.mlps.append(MLP(n_feat, n_hidden, n_class, 1))
+        else:
+            self.mlps.append(MLP(n_feat, n_hidden, n_hidden, 1))
+            for layer in range(self.n_layers-2):
+                self.mlps.append(MLP(n_hidden, n_hidden, n_hidden, 1))
+            self.mlps.append(MLP(n_hidden, n_hidden, n_class, 1))
+        
+    def forward(self, input):
+        x = input[0]
+        adj = input[1]
+
+        if self.learn_eps:
+            for i in range(self.n_layers-1):
+                x = self.mlps[i]((1+self.eps[i])*x + self.spmm(adj,x))
+                x = F.relu(x)
+            x = self.mlps[-1]((1+self.eps[i])*x + self.spmm(adj,x))
+        else:
+            for i in range(self.n_layers-1):
+                x = self.mlps[i](x + self.spmm(adj,x))
+                x = F.relu(x)
+            x = self.mlps[-1](x + self.spmm(adj,x))
+        
+        return x.squeeze(1)
